29b66371ac0a2c6019d75c7e27c89a393d3bac89
Barnaby Kempster <barnabykempster@hotmail.com>
2023-11-28 12:20:16 +0000

    train test split added

---
+++ preproc_base_data/train_test_split.py
@@ -0,0 +1,23 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+
+def shuffle_data(dataframe):
+    df = dataframe.sample(frac=1).reset_index(drop=True)
+    return df
+
+def train_test_split(dataframe):
+    #import within function to avoid errors
+    from sklearn.model_selection import train_test_split
+
+    #shuffle dataset (!!!! using shuffle key word arguement in train_test_split)
+    # df = dataframe.sample(frac=1).reset_index(drop=True)
+
+    #create X and y
+    X = dataframe['text']
+    y = dataframe['binary']
+
+    #instantiate model
+    X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.7, random_state=0,shuffle=True)
+
+    return X_train, X_test, y_train, y_test
