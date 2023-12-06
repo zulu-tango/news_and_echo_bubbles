d5b9498b9fd46b58f3bff2c20c5120b5a6ce5a75
Zoe Tustain <zoe.tustain@gmail.com>
2023-12-06 12:21:25 +0000

    added keys in keyword search

--- models/keyword_search.py
+++ models/keyword_search.py
@@ -3,18 +3,22 @@ import pandas as pd
 import ast
 from datetime import date
 from collections import Counter
-
-
 path = os.getcwd()
 
-def trending_topics():
-    df = pd.read_csv(f'{path}/raw_data/base_table_{date.today()}_stopwords_2.csv')
-    for index, row in enumerate(df.keywords):
-        df.keywords[index] = ast.literal_eval(df.keywords[index])
+def cached_data():
+    data= pd.read_csv(f'{path}/raw_data/base_table_2023-12-05_updated.csv')
+    for index, row in enumerate(data.keywords):
+        data.keywords[index] = ast.literal_eval(data.keywords[index])
+    return data
 
+def trending_topics():
+    df = cached_data()
 
     list_of_keywords = []
     trending_topics = []
+    mask = df['pdate'] == df['pdate'].max()
+    df = df[mask]
+    df.reset_index(drop=True,inplace=True)
     for i in range(len(df.keywords)):
         for key in df.keywords[i].keys():
             list_of_keywords.append((key,))
@@ -31,6 +35,7 @@ def trending_topics():
 
     trending.sort_values(by='count',ascending=False,inplace=True)
     trending.reset_index(drop=True,inplace=True)
+
     topic_1 = trending.word[0]
     topic_2 = trending.word[1]
     topic_3 = trending.word[2]
@@ -48,44 +53,47 @@ def search_keyword(topic):
     returned_articles = []
     for index, row in enumerate(df_ll.keywords):
         if keyword in row:
-            returned_articles.append((df_ll['title'][index],df_ll['pred_class'][index],df_ll['link'][index],df_ll.keywords[index][keyword])) ##TODO get key names out into dataframe
-    output_df_ll = pd.DataFrame(returned_articles,columns=['title','bias','link','keyword_score'])
+            returned_articles.append((df_ll['link'][index],df_ll['pdate'][index],df_ll['title'][index],df_ll['author'][index],df_ll['text'][index],df_ll['urls'][index],df_ll['pred_class'][index],df_ll.keywords[index].keys(),df_ll.keywords[index][keyword])) ##TODO get key names out into dataframe
+    output_df_ll = pd.DataFrame(returned_articles,columns=['link','pdate','title','author','text','urls','pred_class','key','keyword_score'])
     output_df_ll = output_df_ll.sort_values(by=['keyword_score'],ascending=False)
+    output_df_ll.reset_index(drop=True,inplace=True)
 
     returned_articles = []
     for index, row in enumerate(df_l.keywords):
         if keyword in row:
-            returned_articles.append((df_l['title'][index],df_l['pred_class'][index],df_l['link'][index],df_l.keywords[index][keyword]))
-    output_df_l = pd.DataFrame(returned_articles,columns=['title','bias','link','keyword_score'])
+            returned_articles.append((df_l['link'][index],df_l['pdate'][index],df_l['title'][index],df_l['author'][index],df_l['text'][index],df_l['urls'][index],df_l['pred_class'][index],df_l.keywords[index].keys(),df_l.keywords[index][keyword]))
+    output_df_l = pd.DataFrame(returned_articles,columns=['link','pdate','title','author','text','urls','pred_class','key','keyword_score'])
     output_df_l = output_df_l.sort_values(by=['keyword_score'],ascending=False)
+    output_df_l.reset_index(drop=True,inplace=True)
 
     returned_articles = []
     for index, row in enumerate(df_c.keywords):
         if keyword in row:
-            returned_articles.append((df_c['title'][index],df_c['pred_class'][index],df_c['link'][index],df_c.keywords[index][keyword]))
-    output_df_c = pd.DataFrame(returned_articles,columns=['title','bias','link','keyword_score'])
+            returned_articles.append((df_c['link'][index],df_c['pdate'][index],df_c['title'][index],df_c['author'][index],df_c['text'][index],df_c['urls'][index],df_c['pred_class'][index],df_c.keywords[index].keys(),df_c.keywords[index][keyword]))
+    output_df_c = pd.DataFrame(returned_articles,columns=['link','pdate','title','author','text','urls','pred_class','key','keyword_score'])
     output_df_c = output_df_c.sort_values(by=['keyword_score'],ascending=False)
+    output_df_c.reset_index(drop=True,inplace=True)
 
     returned_articles = []
     for index, row in enumerate(df_r.keywords):
         if keyword in row:
-            returned_articles.append((df_r['title'][index],df_r['pred_class'][index],df_r['link'][index],df_r.keywords[index][keyword]))
-    output_df_r = pd.DataFrame(returned_articles,columns=['title','bias','link','keyword_score'])
+            returned_articles.append((df_r['link'][index],df_r['pdate'][index],df_r['title'][index],df_r['author'][index],df_r['text'][index],df_r['urls'][index],df_r['pred_class'][index],df_r.keywords[index].keys(),df_r.keywords[index][keyword]))
+    output_df_r = pd.DataFrame(returned_articles,columns=['link','pdate','title','author','text','urls','pred_class','key','keyword_score'])
     output_df_r = output_df_r.sort_values(by=['keyword_score'],ascending=False)
+    output_df_r.reset_index(drop=True,inplace=True)
 
     returned_articles = []
     for index, row in enumerate(df_rr.keywords):
         if keyword in row:
-            returned_articles.append((df_rr['title'][index],df_rr['pred_class'][index],df_rr['link'][index],df_rr.keywords[index][keyword]))
-    output_df_rr = pd.DataFrame(returned_articles,columns=['title','bias','link','keyword_score'])
+            returned_articles.append((df_rr['link'][index],df_rr['pdate'][index],df_rr['title'][index],df_rr['author'][index],df_rr['text'][index],df_rr['urls'][index],df_rr['pred_class'][index],df_rr.keywords[index].keys(),df_rr.keywords[index][keyword]))
+    output_df_rr = pd.DataFrame(returned_articles,columns=['link','pdate','title','author','text','urls','pred_class','key','keyword_score'])
     output_df_rr = output_df_rr.sort_values(by=['keyword_score'],ascending=False)
+    output_df_rr.reset_index(drop=True,inplace=True)
 
-    return output_df_ll[:2], output_df_l[:2], output_df_c[:2], output_df_r[:2], output_df_rr[:2]
+    return output_df_ll, output_df_l, output_df_c, output_df_r, output_df_rr
 
 def biases():
-    df = pd.read_csv(f'{path}/raw_data/base_table_{date.today()}_stopwords_2.csv')
-    for index, row in enumerate(df.keywords):
-        df.keywords[index] = ast.literal_eval(df.keywords[index])
+    df = cached_data()
 
     mask_ll = df['pred_class'] =='left'
     mask_l = df['pred_class'] =='leans left'
